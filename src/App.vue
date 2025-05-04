1bad2b30dda758c8d5ceac84d055ccaed0f26aae
JCool221 <jcool2939@gmail.com>
2025-05-04 17:24:42 -0400

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import Dropdown from './components/Dropdown.vue';
-import { ref } from 'vue';
+import Footer from './components/Footer.vue';
+// import Dropdown from './components/Dropdown.vue';
+// import { ref } from 'vue';
 import Navbar from './components/Navbar.vue';
-const options = ref<string[]>(['Option 1', 'Option 2', 'Option 3'])
+// const options = ref<string[]>(['Option 1', 'Option 2', 'Option 3'])
 </script>
 
 <template>
-  <Navbar />
-  <nav>
-    <Dropdown :options = "options"/>
-    <!-- this line isn't necessary but i'm leaving it for now for syntax -->
-    <!-- <RouterLink to='/home'>go to the home</RouterLink> -->
-  </nav>
-  <HelloWorld msg="Homebrew Toolbox" />
-  <RouterView />
+  <div class="base-container">
+
+    <Navbar />
+    <nav>
+      <!-- <Dropdown :options = "options"/> -->
+      <!-- this line isn't necessary but i'm leaving it for now for syntax -->
+      <!-- <RouterLink to='/home'>go to the home</RouterLink> -->
+    </nav>
+    <HelloWorld msg="Homebrew Toolbox" />
+    <RouterView />
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
+.base-container{
+  height: 75vh;
+}
 
 </style>
