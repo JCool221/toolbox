193730f8f21d0e5fe2244464d0342af53d8fbd8e
JCool221 <109809337+JCool221@users.noreply.github.com>
2025-05-14 01:54:18 -0400

    integrating modal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
 import Footer from './components/Footer.vue';
-// import Dropdown from './components/Dropdown.vue';
-// import { ref } from 'vue';
 import Navbar from './components/Navbar.vue';
 // const options = ref<string[]>(['Option 1', 'Option 2', 'Option 3'])
 </script>
@@ -11,12 +8,6 @@ import Navbar from './components/Navbar.vue';
   <div class="base-container">
 
     <Navbar />
-    <nav>
-      <!-- <Dropdown :options = "options"/> -->
-      <!-- this line isn't necessary but i'm leaving it for now for syntax -->
-      <!-- <RouterLink to='/home'>go to the home</RouterLink> -->
-    </nav>
-    <!-- <HelloWorld msg="Homebrew Toolbox" /> -->
     <RouterView />
     <Footer />
   </div>
